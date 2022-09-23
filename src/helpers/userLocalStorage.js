e9d5872d994bef370525fb9d45a67d1b05d4615e
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2022-09-23 16:57:54 -0300

    Solucionando problema de lint com o JSON

--- src/helpers/userLocalStorage.js
+++ src/helpers/userLocalStorage.js
@@ -1,6 +1,11 @@
 const USER_KEY = 'user';
 
-export const readUserEmail = () => JSON.parse(localStorage.getItem(USER_KEY));
+export const readUserEmail = () => {
+  if (localStorage.getItem(USER_KEY) === null) {
+    return JSON.stringify({});
+  }
+  return JSON.parse(localStorage.getItem(USER_KEY));
+};
 
 export const saveUserEmail = (email) => localStorage
   .setItem(USER_KEY, JSON.stringify({ email }));
