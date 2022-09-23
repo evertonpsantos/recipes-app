0fc4606aea1b41be90edfd1ce69f1edf01b62bb8
João Antônio Sá <joao.a.csn@gmail.com>
2022-09-23 18:21:17 -0300

    Abrindo PR da Main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+// import rockGlass from './images/rockGlass.svg';
 import React from 'react';
 import './App.css';
-// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import RecipesProvider from './context/RecipesProvider';
