8483e65cf77d8f73e83f6f76f8df7d158773fdd2
carlos eduardo da silva <kadu2229@yahoo.com.br>
2022-09-27 18:08:57 -0300

    tentando

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,12 @@ export default function Profile() {
   return (
     <div>
       <Header />
+      <div>
+        <h1 data-testid="profile-email"> User </h1>
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button type="button" data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer />
     </div>
   );
