4e229d7eb8f75ff6e24a7c524bfe25fca7717808
carlos eduardo da silva <kadu2229@yahoo.com.br>
2022-09-27 18:17:23 -0300

    feito até 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { readUserEmail } from '../helpers/userLocalStorage';
 
 export default function Profile() {
   return (
     <div>
       <Header />
       <div>
-        <h1 data-testid="profile-email"> User </h1>
+        <h1 data-testid="profile-email">{readUserEmail().email}</h1>
         <button type="button" data-testid="profile-done-btn">Done Recipes</button>
         <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button type="button" data-testid="profile-logout-btn">Logout</button>
