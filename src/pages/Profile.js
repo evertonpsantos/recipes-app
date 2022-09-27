668e09bfd9947d2f3c7965ef282d2034f0d04006
carlos eduardo da silva <kadu2229@yahoo.com.br>
2022-09-27 19:15:08 -0300

    feito da 57 até a 62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { readUserEmail } from '../helpers/userLocalStorage';
 
 export default function Profile() {
+  const history = useHistory();
+  const redirectDoneRecipes = () => (
+    history.push('/done-recipes')
+  );
+  const redirectFavoriteRecipes = () => (
+    history.push('/favorite-recipes')
+  );
+  const logout = () => {
+    history.push('/');
+    localStorage.clear();
+  };
   return (
     <div>
       <Header />
       <div>
-        <h1 data-testid="profile-email">{readUserEmail().email}</h1>
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-        <button type="button" data-testid="profile-logout-btn">Logout</button>
+        <h1 data-testid="profile-email">{ readUserEmail().email }</h1>
+        <button
+          onClick={ redirectDoneRecipes }
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          onClick={ redirectFavoriteRecipes }
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ logout }
+        >
+          Logout
+        </button>
       </div>
       <Footer />
     </div>
