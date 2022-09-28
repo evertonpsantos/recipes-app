0af57e7862efd0bc775323db33aa7e82a9040c25
João Antônio Sá <joao.a.csn@gmail.com>
2022-09-28 17:17:46 -0300

    SearchBar - req 10, 11 e 12 concluido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState } from 'react';
+import { useCallback, useEffect, useContext, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import RecipesContext from '../context/RecipesContext';
 import '../style/SearchBar.css';
@@ -12,96 +12,52 @@ export default function SearchBar() {
   const { setFilteredSearch } = useContext(RecipesContext);
   const { pathname } = useLocation();
 
-  // const setMealsEndpoint = () => {
-  //   if (checkedRadioBtn1) {
-  //     const oi = setUrl(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`);
-  //     console.log(oi);
-  //     return oi;
-  //   }
-
-  //   if (checkedRadioBtn2) {
-  //     return setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
-  //   }
-
-  //   if (checkedRadioBtn3 && searchInput.length === 1) {
-  //     return setUrl(`https://www.themealdb.com/api/json/v1/1/filter.php?f=${searchInput}`);
-  //   }
-  //   if (checkedRadioBtn3 && searchInput.length > 1) {
-  //     global.alert('Your search must have only 1 (one) character');
-  //   }
-  // };
-
-  const mealsPath = () => {
+  const mealsPath = useCallback(() => {
     if (checkedRadioBtn1) setUrl(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`);
     else if (checkedRadioBtn2) setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
-    else if (checkedRadioBtn3 && searchInput.length === 1) setUrl(`https://www.themealdb.com/api/json/v1/1/filter.php?f=${searchInput}`);
+    else if (checkedRadioBtn3 && searchInput.length === 1) setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchInput}`);
     else if (checkedRadioBtn3 && searchInput.length > 1) {
       global.alert('Your search must have only 1 (one) character');
     }
-  };
+  }, [checkedRadioBtn1, checkedRadioBtn2, checkedRadioBtn3, searchInput]);
 
-  const drinksPath = () => {
+  const drinksPath = useCallback(() => {
     if (checkedRadioBtn1) setUrl(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchInput}`);
     else if (checkedRadioBtn2) setUrl(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`);
-    else if (checkedRadioBtn3 && searchInput.length === 1) setUrl(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?f=${searchInput}`);
+    else if (checkedRadioBtn3 && searchInput.length === 1) setUrl(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchInput}`);
     else if (checkedRadioBtn3 && searchInput.length > 1) {
       global.alert('Your search must have only 1 (one) character');
     }
-  };
+  }, [checkedRadioBtn1, checkedRadioBtn2, checkedRadioBtn3, searchInput]);
 
-  useEffect(() => (
-    pathname === '/meals' ? mealsPath() : drinksPath()
-  ), [checkedRadioBtn1, checkedRadioBtn2, checkedRadioBtn3, searchInput]);
-
-  // const setDrinksEndpoint = () => {
-  //   if (checkedRadioBtn1) {
-  //     return setUrl(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchInput}`);
-  //   }
-
-  //   if (checkedRadioBtn2) {
-  //     return setUrl(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`);
-  //   }
-
-  //   if (checkedRadioBtn3 && searchInput.length === 1) {
-  //     return setUrl(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?f=${searchInput}`);
-  //   }
-  //   if (checkedRadioBtn3 && searchInput.length > 1) {
-  //     global.alert('Your search must have only 1 (one) character');
-  //   }
-  // };
-
-  // const setCorrectUrl = () => {
-  //   if (pathname === '/meals') {
-  //     setMealsEndpoint();
-  //   }
-  //   if (pathname === '/drink') {
-  //     setDrinksEndpoint();
-  //   }
-  // };
+  useEffect(
+    () => (
+      pathname === '/meals' ? mealsPath() : drinksPath()
+    ),
+    [pathname, mealsPath, drinksPath],
+  );
 
   const checkRadioBtn = ({ target }) => {
     if (target.id === 'ingredient') {
       setCheckedRadioBtn1(!checkedRadioBtn1);
       setCheckedRadioBtn2(false);
       setCheckedRadioBtn3(false);
-      // setCorrectUrl();
     }
 
     if (target.id === 'recipe-name') {
       setCheckedRadioBtn1(false);
       setCheckedRadioBtn2(!checkedRadioBtn2);
       setCheckedRadioBtn3(false);
-      // setCorrectUrl();
     }
     if (target.id === 'first-letter') {
       setCheckedRadioBtn1(false);
       setCheckedRadioBtn2(false);
       setCheckedRadioBtn3(!checkedRadioBtn3);
-      // setCorrectUrl();
     }
   };
 
   const handleSearchClick = async () => {
+    console.log(url);
     const response = await fetch(url);
     const result = await response.json();
     console.log(result);
