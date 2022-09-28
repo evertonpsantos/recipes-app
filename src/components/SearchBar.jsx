bf3bb62166b089cb1e9173f01883af5b10889ca5
João Antônio Sá <joao.a.csn@gmail.com>
2022-09-28 16:36:59 -0300

    SearchBar - req 10 feito, 11 e 12 WIP

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -105,7 +105,9 @@ export default function SearchBar() {
     const response = await fetch(url);
     const result = await response.json();
     console.log(result);
-    setFilteredSearch(result);
+    return pathname === '/meals'
+      ? setFilteredSearch(result.meals)
+      : setFilteredSearch(result.drinks);
   };
 
   return (
