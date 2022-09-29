743788b518e5bab1894d0895b7fc80966b03de4a
Bianca Oura <biancaoura@gmail.com>
2022-09-29 17:23:12 -0300

    add css no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import profileIcon from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
+import profileIcon2 from '../images/profileIcon2.svg';
+import searchIcon2 from '../images/searchIcon2.svg';
+import mealsLogo from '../images/mealsLogo.svg';
+import foodCoverLogo from '../images/foodCoverLogo.svg';
+import writtenLogo from '../images/writtenLogo.svg';
+import '../style/Header.css';
 
 export default function Header() {
   const [showSearchBar, setShowBar] = useState(false);
@@ -29,26 +33,42 @@ export default function Header() {
   const notRenderSearchIcon = ['/profile', '/done-recipes', '/favorite-recipes'];
 
   return (
-    <>
-      <h1 data-testid="page-title">{defineTitle(pathname)}</h1>
-      <button
-        type="button"
-        onClick={ () => history.push('/profile') }
-      >
-        <img src={ profileIcon } alt="profile-icon" data-testid="profile-top-btn" />
-      </button>
-
-      {notRenderSearchIcon.includes(pathname)
-        ? '' : (
+    <header className="header-container">
+      <nav className="navbar-container">
+        <div className="header-logo-container">
+          <img src={ foodCoverLogo } alt="food cover logo" />
+          <img src={ writtenLogo } alt="recipes app" className="header-written-logo" />
+        </div>
+        <div className="header-button-container">
+          {notRenderSearchIcon.includes(pathname)
+            ? '' : (
+              <button
+                className="header-button"
+                type="button"
+                onClick={ () => setShowBar(!showSearchBar) }
+              >
+                <img src={ searchIcon2 } alt="search-icon" data-testid="search-top-btn" />
+              </button>
+            )}
+          { showSearchBar && <SearchBar /> }
           <button
+            className="header-button"
             type="button"
-            onClick={ () => setShowBar(!showSearchBar) }
+            onClick={ () => history.push('/profile') }
           >
-            <img src={ searchIcon } alt="search-icon" data-testid="search-top-btn" />
+            <img src={ profileIcon2 } alt="profile-icon" data-testid="profile-top-btn" />
           </button>
-        )}
+        </div>
+      </nav>
+      <h1
+        className="header-title"
+        data-testid="page-title"
+      >
+        {defineTitle(pathname)}
 
-      { showSearchBar && <SearchBar /> }
-    </>
+      </h1>
+      <img src={ mealsLogo } alt="meals logo" className="header-meals-logo" />
+
+    </header>
   );
 }
