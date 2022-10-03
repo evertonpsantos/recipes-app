f2cc4c3a58552bf5f13a272cc3e062f61c351bc3
Bianca Oura <biancaoura@gmail.com>
2022-10-03 12:22:42 -0300

    req 47

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -3,12 +3,22 @@ import Header from '../components/Header';
 import { readRecipe } from '../helpers/recipeLocalStorage';
 import shareIcon from '../images/shareIcon.svg';
 
+const copy = require('clipboard-copy');
+
 export default function DoneRecipes() {
   const [doneRecipes, setDoneRecipes] = useState([]);
+  const [copyMessage, setCopyMessage] = useState({});
+
   useEffect(() => {
     const recipes = readRecipe('doneRecipes');
     setDoneRecipes(recipes);
   }, []);
+
+  const handleShareButton = (type, id) => {
+    const path = `http://localhost:3000/${type}s/${id}`;
+    copy(path);
+    setCopyMessage({ [id]: 'Link copied!' });
+  };
 
   return (
     <div>
@@ -53,12 +63,14 @@ export default function DoneRecipes() {
             </h5>
             <p data-testid={ `${index}-horizontal-done-date` }>{el.doneDate}</p>
             <button
-              type="button"
               data-testid={ `${index}-horizontal-share-btn` }
+              onClick={ () => handleShareButton(el.type, el.id) }
+              type="button"
               src={ shareIcon }
             >
               <img src={ shareIcon } alt="share-icon" />
             </button>
+            { Object.keys(copyMessage) !== null && <p>{copyMessage[el.id]}</p>}
             { el.tags ? el.tags.map((tag) => (
               <p key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>{tag}</p>
             )) : ''}
